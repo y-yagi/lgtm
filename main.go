61b5affd192fce9722f7b7a4905573008c04c48a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-10-01 16:29:34 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/peterhellberg/giphy"
+)
+
+const LGTM_URL = "http://lgtm.herokuapp.com/"
+
+func openCommand() string {
+	return "gnome-open"
+}
+
+func lgtmMarkdown(url string) string {
+	return "![LGTM](" + url + ")"
+}
+
+func main() {
+	var tag = flag.String("tag", "cat", "Search query term or phrase. Default is 'cat'.")
+	flag.Parse()
+
+	client := giphy.DefaultClient
+	random, err := client.Random([]string{*tag})
+
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+		os.Exit(1)
+	}
+
+	lgtmImageUrl := LGTM_URL + random.Data.ImageURL
+	lgtmMarkdownText := lgtmMarkdown(lgtmImageUrl)
+
+	exec.Command(openCommand(), lgtmImageUrl).Start()
+	fmt.Println(lgtmMarkdownText)
+	os.Exit(0)
+}

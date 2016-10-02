dd8c0f6d8d2bf2d5624d0c3d5acb38d11412cf9f
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-10-02 10:13:15 +0900

    add http response check and download file

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 
@@ -11,6 +13,7 @@ import (
 )
 
 const LGTM_URL = "http://lgtm.herokuapp.com/"
+const MAX_RETRY_COUNT = 3
 
 func openCommand() string {
 	return "gnome-open"
@@ -21,21 +24,63 @@ func lgtmMarkdown(url string) string {
 }
 
 func main() {
+	var lgtmImageUrl string
+	var lgtmFile string
+
 	var tag = flag.String("tag", "cat", "Search query term or phrase.")
 	flag.Parse()
 
-	client := giphy.DefaultClient
-	random, err := client.Random([]string{*tag})
-
+	dir, err := ioutil.TempDir("", "lgtm")
 	if err != nil {
 		fmt.Printf("error: %s\n", err)
 		os.Exit(1)
 	}
 
-	lgtmImageUrl := LGTM_URL + random.Data.ImageURL
-	lgtmMarkdownText := lgtmMarkdown(lgtmImageUrl)
+	for i := 0; i < MAX_RETRY_COUNT; i++ {
+		client := giphy.DefaultClient
+		random, err := client.Random([]string{*tag})
 
-	exec.Command(openCommand(), lgtmImageUrl).Start()
+		if err != nil {
+			fmt.Printf("error: %s\n", err)
+			os.Exit(1)
+		}
+
+		lgtmImageUrl = LGTM_URL + random.Data.ImageURL
+		response, err := http.Get(lgtmImageUrl)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if response.StatusCode != 200 {
+			continue
+		}
+
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		lgtmFile = dir + "/" + random.Data.ID + ".gif"
+		file, err := os.OpenFile(lgtmFile, os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer func() {
+			file.Close()
+		}()
+
+		file.Write(body)
+	}
+
+	if len(lgtmFile) == 0 {
+		fmt.Printf("File generation fails. Please run the command again.\n")
+	}
+
+	exec.Command(openCommand(), lgtmFile).Start()
+
+	lgtmMarkdownText := lgtmMarkdown(lgtmImageUrl)
 	fmt.Println(lgtmMarkdownText)
 	clipboard.WriteAll(lgtmMarkdownText)
 	os.Exit(0)
